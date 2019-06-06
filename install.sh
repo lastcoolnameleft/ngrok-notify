2d3cf0811f536f2c31d6818b84a9d4858366664e
Tommy Falgout <tommy@lastcoolnameleft.com>
2019-06-06 14:38:25 -0500

    Renaming ngrok to remove _

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ if (( $EUID != 0 )); then
 fi
 
 cp ngrok.service /lib/systemd/system/
-cp ngrok_notify.service /lib/systemd/system/
+cp ngrok-notify.service /lib/systemd/system/
 
 mkdir -p /opt/ngrok
 cp ngrok.yaml /opt/ngrok
 cp env.sh /opt/ngrok
-cp ngrok_notify.py /opt/ngrok
+cp ngrok-notify.py /opt/ngrok
 
 systemctl enable ngrok.service
 systemctl restart ngrok.service
-systemctl enable ngrok_notify.service
-systemctl restart ngrok_notify.service
+systemctl enable ngrok-notify.service
+systemctl restart ngrok-notify.service
