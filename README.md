25860c4d70a2a114d2284630dcca6b0a2715df71
Tommy Falgout <tommy@lastcoolnameleft.com>
2025-11-12 15:29:57 -0600

    update for baiiab

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# ngrok-bootup-twilio
-Notification for ngrok bootup using twilio.  
+# ngrok-notify
 
-I created this project for my traveling RaspberryPi project so that I could have a publicly available SSH port for your RaspberryPi, when you didn't know the SSID.
+Notification for ngrok bootup using gmail to notify me when the device is up and ready
+
+I created this project for my traveling RaspberryPi project so that I could have a publicly available SSH port for my RaspberryPi, when you didn't know the SSID.  It's usually tethered to my phone, so I'll have a "reliable" internet connection
 
 ## Installation
 
@@ -10,22 +11,44 @@ I created this project for my traveling RaspberryPi project so that I could have
 To download on a RaspberryPi, follow steps below.  Otherwise, download via https://ngrok.com/download and copy to `/opt/ngrok` directory.
 
 ```shell
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-unzip ngrok-stable-linux-arm.zip
-sudo mkdir /opt/ngrok
-sudo mv ngrok /opt/ngrok/ngrok
+# https://ngrok.com/download/raspberry-pi
+./ngrok --version
+# ngrok version 2.3.41 (last verified)
+sudo mv ngrok /usr/local/bin/ngrok
 ```
 
-2. Install ngrok-notify
+1. Install ngrok-notify
 
 ```
 git clone https://github.com/lastcoolnameleft/ngrok-notify.git
 cd ngrok-notify
 cp env.sh.template env.sh
-cp ngrok.yaml.template ngrok.yaml
+cp ngrok.yaml.template ngrok.yaml 
 # Modify env.sh and ngrok.yaml with relevant values
-sudo pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
+# https://developers.google.com/workspace/gmail/api/quickstart/python (not sure why it needs to be separate, but I tried adding to requirements and kept having issues)
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+# Copy / Build credentials.json
+```
+
+1. Verify ngrok + ngrok-notify
+```
+# Verify ngrok
+/usr/local/bin/ngrok start --all --config /home/pi/ngrok-notify/ngrok.yaml
+
+# Verify ngrok-notify
+/usr/bin/python /home/pi/ngrok-notify/ngrok-notify.py
+
+# https://github.com/googleworkspace/python-samples/blob/main/gmail/quickstart/quickstart.py
+# Run python ./helpers/gmail.py (might need on desktop and then transfer file to RPi)
+
+# After everything works fine and email comes through, install the services
 sudo ./install.sh
+
+sudo service ngrok status
+journalctl --unit ngrok
+sudo service ngrok-notify status
+journalctl --unit ngrok-notify
 ```
 
 ## Validation
@@ -49,5 +72,4 @@ To validate the notify service is working as expected, start it:
 Use these two commands to view the logs from the systemd service that was installed:
 
 `journalctl --unit ngrok`
-
 `journalctl --unit ngrok-notify`
--- ngrok-notify.py
+++ ngrok-notify.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python3
 
 # Download the helper library from https://www.twilio.com/docs/python/install
-import requests
-from twilio.rest import Client
-from retrying import retry
-import os
+import google.auth, os, base64, requests, time
+from email.message import EmailMessage
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-from_phone_no = os.getenv('TWILIO_FROM_PHONE_NO')
-to_phone_no = os.getenv('TWILIO_TO_PHONE_NO')
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-if (not (account_sid and auth_token and from_phone_no and to_phone_no)):
-    print('Required env vars are not set.  Exiting.')
-    exit(1)
+def gmail_send_message():
+  """Create and send an email message
+  Print the returned  message id
+  Returns: Message object, including message id
 
-def send_message():
+  Load pre-authorized user credentials from the environment.
+  TODO(developer) - See https://developers.google.com/identity
+  for guides on implementing OAuth2 for the application.
+  """
+  #creds, _ = google.auth.default()
+  #if os.path.exists("token.json"):
+  creds = None
+  if os.path.exists("/home/pi/ngrok-notify/token.json"):
+    creds = Credentials.from_authorized_user_file("/home/pi/ngrok-notify/token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "/home/pi/ngrok-notify/credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("/home/pi/ngrok-notify/token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("gmail", "v1", credentials=creds)
+    message = EmailMessage()
+
     ssh_public_url, other_public_urls = get_public_urls()
     # splitting because python regex sucks
     # Assuming it looks like this:  'tcp://0.tcp.ngrok.io:19091'
@@ -23,15 +51,31 @@ def send_message():
     [ngrok_hostname, port] = ssh_public_url.split('tcp://')[1].split(':')
     hostname = os.uname().nodename
     body = hostname + " is ready: ssh pi@" + ngrok_hostname + " -p " + port
-    if other_public_urls:
-        body += " Other Endpoints: " + other_public_urls
 
-    print('Sending: ' + body)
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(body=body, from_=from_phone_no, to=to_phone_no)
-    print(message.sid)
+    message.set_content(body)
 
-@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
+    message["To"] = "tommy@lastcoolnameleft.com"
+    message["From"] = "tommy@lastcoolnameleft.com"
+    message["Subject"] = "NGROK NOTIFY"
+
+    # encoded message
+    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+    create_message = {"raw": encoded_message}
+    # pylint: disable=E1101
+    send_message = (
+        service.users()
+        .messages()
+        .send(userId="me", body=create_message)
+        .execute()
+    )
+    print(f'Message Id: {send_message["id"]}')
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    send_message = None
+  return send_message
+
+
 def get_public_urls():
     # Get ngrok tunnel status info
     ngrok_api = 'http://localhost:4040/api/tunnels'
@@ -46,4 +90,6 @@ def get_public_urls():
     #public_url = response.json()['tunnels'][0]['public_url']
     return ssh_public_url, other_public_urls
 
-send_message()
+# Let things get settled
+time.sleep(60)
+gmail_send_message()
